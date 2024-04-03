3da88d3eda0bb9a4e48041d073af3a1990cb9848
ouassma kalli <86204368+koupakalli@users.noreply.github.com>
2024-04-04 01:34:06 +0200

    Add files via upload
    change the place of the photos

---
+++ index.js
@@ -0,0 +1,22 @@
+const apikey = "1f00ac6e3f77d7d1afe011e5fbab3434";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+    const data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
+// You may want to call checkWeather() with a default city here
+// Example:
+// checkWeather("London");
